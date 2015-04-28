1beacdc59546f330f6edda9d111c46d468d1bc84
AlexandraVolochova <s-volohova@yandex.ru>
2015-04-28 17:25:55 +0300

    I created blanks for activities for Wardrobe and Creation new set

---
+++ app/src/main/java/com/example/lenovo/clothesorganizer/CreationActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.clothesorganizer;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class CreationActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation);
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/clothesorganizer/WardrobeActivity.java
@@ -0,0 +1,12 @@
+package com.example.lenovo.clothesorganizer;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class WardrobeActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wardrobe);
+    }
+}
