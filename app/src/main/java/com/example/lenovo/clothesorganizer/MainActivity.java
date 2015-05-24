63d2e4a075ff957c8bde12c6ee45ce040b60b2e8
SofiaAxenowa <sofya.aksenova@phystech.edu>
2015-05-24 21:25:21 +0300

    weather

--- app/src/main/java/com/example/lenovo/clothesorganizer/MainActivity.java
+++ app/src/main/java/com/example/lenovo/clothesorganizer/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.lenovo.clothesorganizer;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.provider.MediaStore;
-import android.os.Environment;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import java.io.File;
-import android.net.Uri;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -23,8 +32,65 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         createDirectory();
-    }
+        final ImageButton button = (ImageButton) findViewById(R.id.buttonRefresh);
 
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                refreshTemperature();
+            }
+        });
+
+        refreshTemperature(); // при запуске программы сразу выводим температуру
+    }
+    // фукция загрузки температуры
+    public String getTemperature(String urlsite)
+    {
+        String matchtemper = "";
+        try
+        {
+            // загрузка страницы
+            URL url = new URL(urlsite);
+            URLConnection conn = url.openConnection();
+            InputStreamReader rd = new InputStreamReader(conn.getInputStream());
+            StringBuilder allpage = new StringBuilder();
+            int n = 0;
+            char[] buffer = new char[40000];
+            while (n >= 0)
+            {
+                n = rd.read(buffer, 0, buffer.length);
+                if (n > 0)
+                {
+                    allpage.append(buffer, 0, n);
+                }
+            }
+            // работаем с регулярным выражением
+            final Pattern pattern = Pattern.compile
+                    ("[^-+0]+([-+0-9]+)[^<]+[^(а-яА-ЯёЁa-zA-Z0-9)]+([а-яА-ЯёЁa-zA-Z ]+)");
+            Matcher matcher = pattern.matcher(allpage.toString());
+            if (matcher.find())
+            {
+                matchtemper = matcher.group(1);
+            }
+            return matchtemper;
+        }
+        catch (Exception e)
+        {
+
+        }
+        return matchtemper;
+    };
+
+    // функция обновления показаний температуры
+    public void refreshTemperature()
+    {
+        final TextView tTemper = (TextView) findViewById(R.id.textviewTemperature);
+        String bashtemp = "";
+        bashtemp = getTemperature("http://be.bashkirenergo.ru/weather/ufa/");
+        tTemper.setText(bashtemp.concat("°")); // отображение температуры
+    };
     //Методы для меню
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -85,9 +151,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_CODE_PHOTO) {
             if (resultCode == RESULT_OK) {
-             Intent i = new Intent(MainActivity.this, AdditionActivity.class);
-             i.putExtra("com.example.lenovo.clothesorganizer.PHOTO_PATH", photoPath);
-             startActivity(i);
+                Intent i = new Intent(MainActivity.this, AdditionActivity.class);
+                i.putExtra("com.example.lenovo.clothesorganizer.PHOTO_PATH", photoPath);
+                startActivity(i);
             }
 
 
