8f26f6043637720a9816df50bbcaf64d42482181
AlexandraVolochova <s-volohova@yandex.ru>
2015-05-24 15:06:14 +0300

    Я запилила простейший ListView в WardrobeActivity

--- app/src/main/java/com/example/lenovo/clothesorganizer/WardrobeActivity.java
+++ app/src/main/java/com/example/lenovo/clothesorganizer/WardrobeActivity.java
@@ -1,20 +1,70 @@
 package com.example.lenovo.clothesorganizer;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class WardrobeActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wardrobe);
+        final ListView listview = (ListView) findViewById(R.id.listview);
+
+        //Создаем текстовый массив, состоящий из слов:
+        final String[] values = new String[]{
+                "Up", "Down", "Footwear",
+                "Accessories"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+
     }
 
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_wardrobe, menu);
+        getMenuInflater().inflate(R.menu.menu_addition, menu);
         return true;
     }
 
@@ -26,5 +76,5 @@ public class WardrobeActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-}
 
+}
