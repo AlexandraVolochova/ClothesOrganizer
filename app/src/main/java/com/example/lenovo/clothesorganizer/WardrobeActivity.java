f4106cb457005371d80de5a3e8720cbca9b06d42
AlexandraVolochova <s-volohova@yandex.ru>
2015-05-24 17:22:52 +0300

    Создала новую активити, в которой будут отображаться вещи определенной категории (в зависимости от передаваемого интента), сделала на нее переход при на жатии на категорию в ListView в WardrobeActivity

--- app/src/main/java/com/example/lenovo/clothesorganizer/WardrobeActivity.java
+++ app/src/main/java/com/example/lenovo/clothesorganizer/WardrobeActivity.java
@@ -2,6 +2,7 @@ package com.example.lenovo.clothesorganizer;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -9,7 +10,6 @@ import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 import android.view.View;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -34,6 +34,16 @@ public class WardrobeActivity extends ActionBarActivity {
         final StableArrayAdapter adapter = new StableArrayAdapter(this,
                 android.R.layout.simple_list_item_1, list);
         listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,int position, long id) {
+                String item = (String) parent.getItemAtPosition(position);
+                Intent i = new Intent(WardrobeActivity.this, WardrobeCategoryActivity.class);
+                i.putExtra("com.example.lenovo.clothesorganizer.THING_TYPE", item);
+                startActivity(i);
+            }});
 
 
     }
@@ -61,7 +71,10 @@ public class WardrobeActivity extends ActionBarActivity {
             return true;
         }
 
+
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_addition, menu);
---
+++ app/src/main/java/com/example/lenovo/clothesorganizer/WardrobeCategoryActivity.java
@@ -0,0 +1,49 @@
+package com.example.lenovo.clothesorganizer;
+
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class WardrobeCategoryActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wardrobe_category);
+    }
+
+    //Методы для меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
